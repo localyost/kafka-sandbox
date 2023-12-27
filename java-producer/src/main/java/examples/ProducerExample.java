d0b5bfb11e1202f5933d5e2261cc945bacc2e61f
p.yost <peter.yost@intive.com>
2023-12-27 11:49:14 +0100

    clean up imports

--- java-producer/src/main/java/examples/ProducerExample.java
+++ java-producer/src/main/java/examples/ProducerExample.java
@@ -5,11 +5,7 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import utils.ConfigUtils;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.Random;
 
